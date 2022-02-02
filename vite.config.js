2b8ba76a987ac94fb612f39eb8077c040883b3ae
David Rachwalik <david.rachwalik@outlook.com>
2022-02-02 00:27:59 -0600

    vite config updated to auto launch

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // Auto launch browser: https://vitejs.dev/config/#server-open
+  server: { open: '/' },
 });
