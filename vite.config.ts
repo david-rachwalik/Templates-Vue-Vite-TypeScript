1b8e571adac08a041ce190264358fbe6b9ec9862
David Rachwalik <david.rachwalik@outlook.com>
2022-02-02 06:01:56 -0600

    have vite open browser on startup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
 export default defineConfig({
   plugins: [vue()],
 
   // Auto launch browser: https://vitejs.dev/config/#server-open
-  //   server: { open: '/' },
-  // instead, currently using VSCode Extension: Vite, by Anthony Fu
+  server: { open: '/' },
+  // decided against VSCode Extension: Vite, by Anthony Fu
 
   root: 'src',
   publicDir: 'assets/public',
