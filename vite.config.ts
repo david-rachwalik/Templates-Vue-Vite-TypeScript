9ed849dfed5f89e52e2cc8ad839c0714228cfebc
David Rachwalik <david.rachwalik@outlook.com>
2022-02-02 04:01:13 -0600

    Vite configured for wwwroot

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
 export default defineConfig({
   plugins: [vue()],
+
   // Auto launch browser: https://vitejs.dev/config/#server-open
   //   server: { open: '/' },
   // instead, currently using VSCode Extension: Vite, by Anthony Fu
+
+  root: 'src',
+  publicDir: 'assets/public',
+  build: {
+    outDir: '../wwwroot',
+    emptyOutDir: true, // confirm because outDir jumps up from root
+  },
 });
